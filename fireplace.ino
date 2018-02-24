1c4de39505095b63f868f2869398d6c1ab3f94db
Will Ochandarena <wochanda@gmail.com>
2018-02-24 13:23:52 -0500

    Update fireplace.ino

--- fireplace.ino
+++ fireplace.ino
@@ -11,20 +11,14 @@ void loop() {
 }
 
 int fireToggle(String command) {
-    /* Particle.functions always take a string as an argument and return an integer.
-    Since we can pass a string, it means that we can give the program commands on how the function should be used.
-    In this case, telling the function "on" will turn the LED on and telling it "off" will turn the LED off.
-    Then, the function returns a value to us to let us know what happened.
-    In this case, it will return 1 for the LEDs turning on, 0 for the LEDs turning off,
-    and -1 if we received a totally bogus command that didn't do anything to the LEDs.
-    */
-
     if (command=="on") {
         digitalWrite(relay1,HIGH);
+        Particle.publish("fire", "on");
         return 1;
     }
     else if (command=="off") {
         digitalWrite(relay1,LOW);
+        Particle.publish("fire", "off");
         return 0;
     }
     else {
